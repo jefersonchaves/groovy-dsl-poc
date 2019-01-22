a0e4efde61925a8b32ab30be18d6efae7424efae
Jéferson Chaves <1755734+jefersonchaves@users.noreply.github.com>
2019-01-22 21:41:52 +0000

    Update buildWrapperTest.groovy
    Leaving only script load

--- test/buildWrapperTest.groovy
+++ test/buildWrapperTest.groovy
@@ -4,9 +4,7 @@ import com.lesfurets.jenkins.unit.BasePipelineTest
 class buildWrapperTest extends BasePipelineTest {
         
         @Test
-        void should_execute_without_errors() throws Exception {
+        public void should_execute_without_errors() throws Exception {
             def script = loadScript("vars/buildWrapper.groovy")
-            script.execute()
-            printCallStack()
         }
 }
