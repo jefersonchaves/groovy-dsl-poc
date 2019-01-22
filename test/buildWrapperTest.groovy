56ff06caf51ad1c2b03d4d8a28b3a8cf673631b8
Jéferson Chaves <1755734+jefersonchaves@users.noreply.github.com>
2019-01-22 21:46:45 +0000

    Update buildWrapperTest.groovy
    Call setup

--- test/buildWrapperTest.groovy
+++ test/buildWrapperTest.groovy
@@ -1,10 +1,17 @@
-import org.junit.Test
 import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Before
+import org.junit.Test
 
 class buildWrapperTest extends BasePipelineTest {
         
-        @Test
-        public void should_execute_without_errors() throws Exception {
-            def script = loadScript("vars/buildWrapper.groovy")
-        }
+    @Override
+    @Before
+    public void setUp() throws Exception {
+      super.setUp()
+    }
+
+    @Test
+    public void should_execute_without_errors() throws Exception {
+      def script = loadScript("vars/buildWrapper.groovy")
+    }
 }
