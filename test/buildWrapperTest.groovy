14223bad5931465735b136ec12f57e8793989ede
Jéferson Chaves <1755734+jefersonchaves@users.noreply.github.com>
2019-01-22 22:30:45 +0000

    Update buildWrapperTest.groovy
    Testing assertion

--- test/buildWrapperTest.groovy
+++ test/buildWrapperTest.groovy
@@ -2,6 +2,8 @@ import com.lesfurets.jenkins.unit.BasePipelineTest
 import org.junit.Before
 import org.junit.Test
 
+import static org.junit.Assert.*
+
 class buildWrapperTest extends BasePipelineTest {
         
     @Override
@@ -14,6 +16,10 @@ class buildWrapperTest extends BasePipelineTest {
     public void should_execute_without_errors() throws Exception {
         def script = loadScript("vars/buildWrapper.groovy")
         script.call()
-        assert false
+        assertThat(helper.callStack.findAll { call ->
+            call.methodName == "echo"
+        }.any { call ->
+            callArgsToString(call).contains("Hello there")
+        }).isTrue()
     }
 }
